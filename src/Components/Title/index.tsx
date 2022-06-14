4976410ae32aaed1837d89e6e4183a5ed379cf87
Wagnetto <wagnetto@protonmail.com>
2022-06-14 17:35:45 -0300

    Component Title updated

---
+++ src/Components/Title/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as S from './styles';
+
+const TituloTabela = () => {
+  return <S.StyledTitulo>Lista de Pedidos em andamento</S.StyledTitulo>;
+};
+
+export default TituloTabela;
---
+++ src/Components/Title/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { fonteTitulos } from '../UI/variaveis';
+
+export const StyledTitulo = styled.h2`
+  font-family: ${fonteTitulos};
+  margin-bottom: 1.5rem;
+`;
